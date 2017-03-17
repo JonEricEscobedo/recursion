5db3c926685de1747f216ade07a48a19dc194397
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-03-16 17:42:58 -0700

    getElementsByClassName now finding class in body element
    The function successfully finds the className in <body> element. Now
    to make it recurse through the <body> children...
    
    -JEE

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  // Hack Reactor tips... use:
+  //   document.body
+  //   <element>.childNodes
+  //   <element>.classList
+  //
+  // Initialize a new array that will store elements that have className
+  // Create a sub-function that will search DOM for className
+  // Send <body> contents through sub-function (document.body)
+  //   If className is found
+  //     Send <element> to new array
+  //   Else
+  //     Recursively send children of <body> to sub-function until
+  //     className is found
+
+  var result = [];
+  var searchArea = document.body
+
+  function findClassName(element) {
+  	if (element.classList.length > 0) {
+  		for (var idx = 0; idx < element.classList.length; idx++) {
+  			if (element.classList[idx] === className) {
+  				result.push(element);
+  			}
+  		}
+  	}
+  }
+
+  findClassName(searchArea);
+  console.log(result);
+  return result;
 };
